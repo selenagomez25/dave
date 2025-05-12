0fc4df3fc8af0cbf1d685a16462525ddcbc3aafa
pathos <gvjsgsisgsj@gmail.com>
2025-05-12 12:28:36 -0500

    initial commit

---
+++ loader.lua
@@ -0,0 +1,24 @@
+local scripts = {
+    [2788229376] = "url", -- da hood
+}
+
+local gameId = game and game.PlaceId or 0
+local scriptUrl = scripts[gameId]
+
+if scriptUrl then
+    local success, result = pcall(function()
+        return game:HttpGet(scriptUrl)
+    end)
+    if success then
+        local loadedFunc, err = loadstring(result)
+        if loadedFunc then
+            loadedFunc()
+        else
+            warn("failed to load: " .. tostring(err))
+        end
+    else
+        warn("failed ot fetch from url: " .. tostring(result))
+    end
+else
+    warn("no scsript for game id: " .. tostring(gameId))
+end
